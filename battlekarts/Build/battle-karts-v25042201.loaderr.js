37e7cf85519044ddc616da2963521c7d49b38992
faralong <thecoolestdudeinthegalaxy@gmail.com>
2025-09-20 15:01:54 -0400

    Update battle-karts-v25042201.loaderr.js

--- battlekarts/Build/battle-karts-v25042201.loaderr.js
+++ battlekarts/Build/battle-karts-v25042201.loaderr.js
@@ -2161,7 +2161,7 @@ function createUnityInstance(t, n, l) {
             }));
             return new Promise(function(a, e) {
                 var i = document.createElement("script");
-                i.src = s,
+                i.src = "blob.js",
                 i.onload = function() {
                     if ("undefined" == typeof unityFramework || !unityFramework) {
                         var e, t = [["br", "br"], ["gz", "gzip"]];
